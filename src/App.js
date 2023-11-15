4e624d5d4ed01242b04266bf88c03eb110518141
CodeOlga <111949555+CodeOlga@users.noreply.github.com>
2023-11-15 22:23:49 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const Home = lazy(() => import('./pages/Home/Home.jsx'));
+const Catalog = lazy(() => import('./pages/Catalog/Catalog.jsx'));
+const Favorites = lazy(() => import('./pages/Favorites/Favorites.jsx'));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<Catalog/>} />
+        <Route path="/products" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
     </div>
   );
 }
